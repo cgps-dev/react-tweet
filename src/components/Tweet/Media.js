81b11dc307646aff6b44eb3da65b61e783dcdfde
Richard Goater <richard.goater27@gmail.com>
2019-01-17 14:23:02 +0000

    Clean dependecies and remove video

--- src/components/Tweet/Media.js
+++ src/components/Tweet/Media.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Photos from './Photos'
-import Video from './Video'
+// import Video from './Video'
 
 class Media extends React.Component {
   render () {
     switch (this.props.media[0].type) {
       case 'photo':
         return <Photos {... this.props} />
-      case 'video':
-        return <Video {... this.props} />
+      // case 'video':
+      //   return <Video {... this.props} />
       case 'animated_gif':
         return <Video gif={true} {... this.props} />
       default:
